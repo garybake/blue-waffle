90e8ddd2ded46e8a6b7dcd5f5e0a8765507ea476
Gary Bake <gary.bake@ukfuels.co.uk>
2020-07-10 08:36:46 +0100

    Added training data generator

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # blue-waffle
-Source set of x type images to train a model and then find them for sale on ebay/fb
+Source set of x type images to train a model and then find them for sale on ebay/fb  
+
+
+Generate training data
+======================
+
+Generate enough source images. i.e. if you wanted to train a model to differentiate swans from sausages.  
+
+```bash
+$ python get_images.py swan sausage
+```
---
+++ get_images.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from google_images_download import google_images_download
+from PIL import Image
+
+
+def download_images(pos_img, neg_img, limit=100, output_dir="image_data"):
+    """Download images to folder."""
+    response = google_images_download.googleimagesdownload()
+
+    arguments = {
+        "keywords": f"{pos_img},{neg_img}",
+        "limit": limit,
+        "print_urls": False,
+        "output_directory": output_dir,
+    }
+    paths = response.download(arguments)
+    return paths
+
+
+def correct_image_types(paths):
+    """Fix image types.
+
+    Remove original files
+    Convert webp to jpg
+    """
+    for keyword in paths:
+        for image_file in paths[keyword]:
+            if image_file.endswith(".webp"):
+                print(f"Fixing: {image_file}")
+                im = Image.open(image_file).convert("RGB")
+                im.save(image_file + "_fix.jpg", "jpeg")
+                os.remove(image_file)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        paths, _ = download_images(sys.argv[1], sys.argv[2])
+        correct_image_types(paths)
+    else:
+        print("Error: Invalid parameters")
+        print('get_images.py "<positive category>" "<negative category"')
